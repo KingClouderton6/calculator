618cdce0a4e40e4e49454432131cd10449bbea01
KingClouderton6 <islandintheclouds@outlook.com>
2021-11-30 16:39:23 +0000

    Added on operate function that takes an operands and calls the respective operation on it

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const operate = function(operator, a, b){
+    if (operator === "+"){
+        add(a, b);
+    }
+    if (operator === "-"){
+        subtract(a, b);
+    }
+    if (operator === "*"){
+        multiply(a, b);
+    }
+    if (operator === "/"){
+        divide(a, b);
+    }
+};
